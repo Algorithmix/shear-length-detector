c408fc99900d99aaf0b6048fcff861db4a657079
Jacob Moisan <jmoisan216@gmail.com>
2013-04-04 22:06:37 -0400

    Fixed bug

--- Shear Detection/Shear Detection/Program.cs
+++ Shear Detection/Shear Detection/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using AForge.Imaging;
 
 namespace Shear_Detection
 {
@@ -11,19 +12,24 @@ namespace Shear_Detection
     {
         static void Main(string[] args)
         {
-            Bitmap shred = new Bitmap("C:\\Users\\Jacob\\Pictures\\full_shreds\\image6.jpg");
-            int top = (int)(shred.Height * 0.25);
-            int bottom = (int)(shred.Height * 0.75);
-            
-            //use function to draw blue line at point of interest
-            for (int j = top; j <= bottom; j++)
+            string [] Files = System.IO.Directory.GetFiles("C:\\Users\\Jacob\\Pictures\\ALL_SHRED\\");
+            foreach (string s in Files)
             {
-                int l = getShearLength(shred, j, true);
-                int r = getShearLength(shred, j, false);
-                shred.SetPixel(l, j, Color.Blue);
-                shred.SetPixel(r, j, Color.Blue);
+                Bitmap shred = new Bitmap(s);
+
+                int top = (int)(shred.Height * 0.25);
+                int bottom = (int)(shred.Height * 0.75);
+
+                //use function to draw blue line at point of interest
+                for (int j = top; j <= bottom; j++)
+                {
+                    int l = getShearLength(shred, j, true);
+                    int r = getShearLength(shred, j, false);
+                    shred.SetPixel(l, j, Color.Blue);
+                    shred.SetPixel(r, j, Color.Blue);
+                }
+                shred.Save(s.Replace("ALL_SHRED", "SHEAR_DETECTED"));
             }
-            shred.Save("C:\\Users\\Jacob\\Pictures\\shearOutput.png");
         }
 
         static int getShearLength(Bitmap bmp, int yCoord, bool isLeft)
@@ -32,11 +38,13 @@ namespace Shear_Detection
             const int textDiff = 300;   //if color sum difference is >= 300 then we are at text
             const int paperDiff = 60;   //if color sum difference is <= 60 then we are at paper
             int i = 0;
+            int edgeDist;
 
             if (isLeft)
             {
                 //get to start of edge
                 i = 0;
+                edgeDist = 0;
                 Color c = bmp.GetPixel(i, yCoord);
                 while (c.A == 0)
                 {
@@ -53,8 +61,13 @@ namespace Shear_Detection
                     int diff = refSum - currentSum;
                     if (diff >= textDiff || diff <= paperDiff)
                         end = true;
+                    else if (edgeDist >= 20)
+                    {
+                        end = true;
+                    }
                     else
                     {
+                        edgeDist++;
                         i++;
                         c = bmp.GetPixel(i, yCoord);
                     }
@@ -73,6 +86,8 @@ namespace Shear_Detection
 
                 //find width
                 bool end = false;
+                //counter to make sure we dont go too far
+                edgeDist = 0;
                 int refSum = 255 * 3;
                 while (!end)
                 {
@@ -80,8 +95,13 @@ namespace Shear_Detection
                     int diff = refSum - currentSum;
                     if (diff >= textDiff || diff <= paperDiff)
                         end = true;
+                    else if (edgeDist >= 20)
+                    {
+                        end = true;
+                    }
                     else
                     {
+                        edgeDist++;
                         i--;
                         c = bmp.GetPixel(i, yCoord);
                     }
